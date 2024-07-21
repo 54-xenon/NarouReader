996b9c67100f7e0582bd7fd2d964554d404c6797
54-xenon <congas.rabbi_0b@icloud.com>
2024-07-21 16:53:24 +0900

    戻るボタンを追加

--- lib/screens/detailPage.dart
+++ lib/screens/detailPage.dart
@@ -6,13 +6,11 @@ class Detailpage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("新しいページ"),
-          backgroundColor: Colors.blue,
-          automaticallyImplyLeading: true,
-        ),
+    return Scaffold(
+      // MaterialAppで返すと戻るボタンが表示され無いから直接scaffoldにする
+      appBar: AppBar(
+        title: const Text('新しいページ'),
+        backgroundColor: Colors.blue,
       ),
     );
   }
