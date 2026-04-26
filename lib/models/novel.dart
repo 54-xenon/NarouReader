7035fecc8a9372f6c94110713a4174ceb1da5767
54-xenon <congas.rabbi_0b@icloud.com>
2026-04-26 17:03:13 +0900

    取得するプロパティの表示・取得を追加

--- lib/models/novel.dart
+++ lib/models/novel.dart
@@ -1,10 +1,36 @@
 
+// 追加で表示したいデータ
+/*
+  - biggenre -> 大ジャンル。Integer型
+  - genre -> ジャンル
+  - keyword -> キーワード、タグみたいなもの
+  - general_firstup -> 初回掲載日
+  - general_lastup -> 最新掲載日
+  - novel_type -> 連載か短編か？
+  - length -> 作品文字数
+  - time -> 読書時間(分単位)
+  - global_point -> 総合評価ポイント
+  - fav_novel_cunt -> ブックマーク数
+ */
+
+
 // モデルクラス
 class Novel {
   final String title; //作者名
   final String story; //作品のあらすじ
   final String writer; //作者名
   final String ncode; //Nコード
+  final int biggenre; //大ジャンル
+  final int genre; // ジャンル
+  final String keywords; //キーワード。検索用の変数が同じ名前だから頭文字を変えた
+  final int novelType; // 種類
+  final int length; // 小説の文字数
+  final int time; //読書の所要時間
+  final int globalPoint; // 総合評価ポイント
+  final int favNovelCunt; // ブックマーク数
+  final DateTime generalFirstup; // 初回掲載日
+  final DateTime generalLastup; // 最新掲載日
+
 
 
   Novel({
@@ -12,6 +38,16 @@ class Novel {
     required this.story,
     required this.writer,
     required this.ncode,
+    required this.biggenre,
+    required this.genre,
+    required this.keywords,
+    required this.novelType,
+    required this.length,
+    required this.time,
+    required this.globalPoint,
+    required this.favNovelCunt,
+    required this.generalFirstup,
+    required this.generalLastup,
   });
 
   factory Novel.fromJson(Map<String, dynamic> json) {
@@ -20,6 +56,16 @@ class Novel {
       story: json['story'],
       writer: json['writer'],
       ncode: json['ncode'],
+      biggenre: json['biggenre'],
+      genre: json['genre'],
+      keywords: json['keyword'],
+      novelType: json['novel_type'],
+      length: json['length'],
+      time: json['time'],
+      globalPoint: json['global_point'],
+      favNovelCunt: json["fav_novel_cnt"],
+      generalFirstup: DateTime.parse(json['general_firstup']),
+      generalLastup: DateTime.parse(json['general_lastup']),
     );
   }
 }
--- lib/screens/detailPage.dart
+++ lib/screens/detailPage.dart
@@ -26,6 +26,8 @@ class _DetailPageState extends State<DetailPage> {
         title: Text(l10n.detailTitle),
         elevation: 1,
       ),
+
+      // 追加ボタンの処理
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           await dbHelper.insertItem(
@@ -42,37 +44,112 @@ class _DetailPageState extends State<DetailPage> {
         },
         child: const Icon(Icons.add),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SelectableText(
-              widget.novel.title,
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            SelectableText(
-              l10n.authorLabel(widget.novel.writer),
-              style: const TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 10),
-            SelectableText(
-              l10n.urlLabel(widget.novel.ncode),
-              style: const TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 10),
-            SelectableText(
-              l10n.ncodeLabel(widget.novel.ncode),
-              style: const TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 20),
-            const Divider(),
-            SelectableText(
-              widget.novel.story,
-              style: const TextStyle(fontSize: 18),
-            ),
-          ],
+      body: SelectionArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // タイトル
+              Text(
+                widget.novel.title,
+                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+
+              // 著者名
+              Text(
+                l10n.authorLabel(widget.novel.writer),
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 10),
+
+              // Nコード
+              Text(
+                l10n.ncodeLabel(widget.novel.ncode),
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 10),
+
+              // 大ジャンル-> 最初の形はInt型。mapで対応するカテゴリをStringで将来的には表示したい
+              Text(
+                "大カテゴリ: ${widget.novel.biggenre}",
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 10),
+
+              // 少ジャンル
+              Text(
+                "カテゴリ: ${widget.novel.genre}",
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 10),
+
+              // キーワード
+              Text(
+                "キーワード: ${widget.novel.keywords}",
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 10),
+
+              // 種類
+              Text(
+                "種類: ${widget.novel.novelType}",
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 10),
+
+              // 文字数
+              Text(
+                "文字数: ${widget.novel.length}",
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 10),
+
+              // 所要時間
+              Text(
+                "読書時間: ${widget.novel.time}分",
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 10),
+              
+              // 創業評価ポイント
+              Text(
+                "創業評価ポインt: ${widget.novel.globalPoint}pt",
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 10),
+
+              //ブックマーク数
+              Text(
+                "ブックマーク数: ${widget.novel.favNovelCunt}",
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 10),
+
+              // 初回掲載日
+              Text(
+                "初回掲載日${widget.novel.generalFirstup}",
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 10),
+              
+              // 最終掲載日
+              Text(
+                "最終掲載日${widget.novel.generalLastup}",
+                style: const TextStyle(fontSize: 18),
+              ),
+              const SizedBox(height: 10),
+
+              
+              const Divider(),
+              // story
+              Text(
+                widget.novel.story,
+                style: const TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
         ),
       ),
     );
